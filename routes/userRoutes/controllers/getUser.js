18453fc5110ac65f2f772ad2f8bcba0de9337f4f
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-09-18 09:29:03 +0530

    get user and users end points created

--- routes/userRoutes/controllers/getUser.js
+++ routes/userRoutes/controllers/getUser.js
@@ -1,7 +1,27 @@
 const User = require('../../../models/User');
 
-const getUsers = async (req,res) => {
-    
+const getUsers = async (req, res) => {
+    const users = await User.find();
+    res.status(200).json(users);
 }
 
-module.exports = getUsers;
+const getUser = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+    const user = await User.findOne({ _id: id });
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    } else {
+        res.status(200).json(user);
+
+    }
+    }catch (error) {
+        res.status(500).json({ message: message.error });
+    }
+
+
+
+}
+
+module.exports = { getUsers, getUser };
--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -6,12 +6,17 @@ const register = require('./controllers/register');
 const emailVerify = require('./controllers/emailVerify');
 const login = require('./controllers/login');
 const forgetPassword = require('./controllers/forgetPassword');
+const getUsers = require('./controllers/getUser').getUsers;
+const getUser = require('./controllers/getUser').getUser;
 
 // create route 
 router.post('/register', register);
 router.get('/verify/:token', emailVerify);
 router.post('/login', login);
 router.post('/forgetpassword', forgetPassword);
+router.get('/users', getUsers);
+router.get('/user/:id', getUser);
+
 
 // export router
 module.exports = router;
