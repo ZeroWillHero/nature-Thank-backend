c5ce956b7cf158ed7fa19d5ee75d6ec67cb3be7b
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-09-18 17:15:48 +0530

    add Endpoindts for the password resetting scripts

--- routes/userRoutes/index.js
+++ routes/userRoutes/index.js
@@ -8,6 +8,8 @@ const login = require('./controllers/login');
 const forgetPassword = require('./controllers/forgetPassword');
 const getUsers = require('./controllers/getUser').getUsers;
 const getUser = require('./controllers/getUser').getUser;
+const changePassword = require('./controllers/passwordResetEmail');
+const passwordReset = require('./controllers/resetPassword');
 
 // create route 
 router.post('/register', register);
@@ -16,6 +18,8 @@ router.post('/login', login);
 router.post('/forgetpassword', forgetPassword);
 router.get('/users', getUsers);
 router.get('/user/:id', getUser);
+router.get('/changepassword/:token',changePassword);
+router.post('/resetpassword',passwordReset);
 
 
 // export router
