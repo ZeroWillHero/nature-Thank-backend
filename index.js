6df0bc1c935ba9f8d25abc1bf2403b5f2c457d67
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-26 12:22:36 +0530

    admin reg UI connected

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 
 // import routes 
 const userRoutes = require('./routes/userRoutes');
+const productRoutes = require('./routes/products/product.routes');
 
 
 // connect to database 
@@ -17,9 +19,12 @@ app.use((req,res,next) => {
     console.log(`Method :) ${req.method} URL :) ${req.url} , Body : ${req.body.email}`);next();
 });
 
+app.use(cors());
+
 
 // Routes 
 app.use('/api/user', userRoutes);
+app.use('/api/products', productRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
