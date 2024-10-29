9ab629d4ae37a2b44a015965a43b3859396dfd1f
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-29 13:18:10 +0530

    productDelete end point created

--- routes/products/product.routes.js
+++ routes/products/product.routes.js
@@ -4,12 +4,15 @@ const uploadImages = require('./../../middlewares/imageUpload');
 const getProducts = require('./controllers/getProduct').getProducts;
 const getProduct = require('./controllers/getProduct').getProduct;
 const updateProduct = require('./controllers/updateProduct');
+const deleteProduct = require('./controllers/deleteProduct');
 
 
 router.post('/add', uploadImages.array('images', 10), addProduct);
 router.get('/get',getProducts);
 router.get('/get/:id',getProduct);
 router.patch('/update/:id',uploadImages.array('images', 10),updateProduct);
+router.delete('/delete/:id',deleteProduct);
+
 
 
 module.exports = router;
