6d21237b202e335e55754d4cb4ec501ce042e5c9
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-30 14:17:58 +0530

    fix bugs on userAuth.js

--- middlewares/userAuth.js
+++ middlewares/userAuth.js
@@ -2,23 +2,21 @@ const jwt = require('jsonwebtoken');
 const User = require('./../models/User');
 
 const userAuth = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token){
-        return res.status(401).send({error: 'Access Denied'});
+    const authHeader = req.header('Authorization');
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).send({ error: 'Access Denied' });
     }
 
-    try {
-        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-        const user = User.findOne({_id: verified._id});
-        if (!user){
-            return res.status(401).send({error: 'Access Denied'});
-        }
 
-        next();
-    
-    }catch(error){
-        return res.status(400).send({error: error});
+    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+    const user = User.findOne({ email : verified.email });
+    if (!user) {
+        return res.status(401).send({ error: 'Access Denied' });
     }
+
+    next();
+
 }
 
 module.exports = userAuth;
