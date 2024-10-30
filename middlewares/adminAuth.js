47d54f88d384c46235dcbbee938a12f26b09aaba
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-30 14:17:33 +0530

    fix bugs on adminAuth.js

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -4,7 +4,6 @@ const User = require('./../models/User');
 const adminAuth = async (req, res, next) => {
     const authHeader = req.header('Authorization');
     const token = authHeader.split(' ')[1];
-    console.log(jwt.verify(token, process.env.JWT_SECRET));
     if (!authHeader) {
         return res.status(401).send({ error: 'Access Denied: No token provided' });
     }
