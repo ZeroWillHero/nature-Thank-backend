093d8d75ea51d4d1466a2e384c44ade440d7a777
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-31 21:20:26 +0530

    cart update fixed

--- routes/cart/controllers/updateCart.js
+++ routes/cart/controllers/updateCart.js
@@ -1,22 +1,28 @@
 const Cart = require('../../../models/Cart');
 
-const updateCart = async (req,res) => {
+const updateCart = async (req, res) => {
     const { id } = req.params;
     const { quantity } = req.body;
 
     try {
-        const cart = await Cart.findOne({_id : id});
+        // Populate the product field to access its price
+        const cart = await Cart.findOne({ _id: id }).populate('product');
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+
         const updatedCart = await Cart.findOneAndUpdate(
             { _id: id },
             {
-                $set: { quantity: quantity, total: cart.product.price * quantity }
+                $inc: { quantity: quantity, total: cart.product.price * quantity }
             },
             { new: true }
         );
-        res.status(200).json({ savedCart : updatedCart });
-    }catch(error){
+
+        res.status(200).json({ savedCart: updatedCart });
+    } catch (error) {
         res.status(500).json({ error });
     }
-}
+};
 
 module.exports = updateCart;
