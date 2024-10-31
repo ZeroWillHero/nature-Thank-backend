18e963596faa0adca53359d18399c0c2b04d5aba
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-10-31 18:58:06 +0530

    get cart route implemented

--- routes/cart/cart.routes.js
+++ routes/cart/cart.routes.js
@@ -1,10 +1,18 @@
 const router = require('express').Router();
+const adminAuth = require('./../../middlewares/adminAuth');
+
 const addToCart = require('./controllers/addCart');
 const deleteCart = require('./controllers/deleteCart');
 const updateCart = require('./controllers/updateCart');
+const getCart = require('./controllers/getCart').getCart;
+const getCarts = require('./controllers/getCart').getAllCart;
+
 
 router.post('/add', addToCart);
 router.delete('/delete/:id',deleteCart);
 router.patch('/update/:id',updateCart);
+router.get('/get/:id',getCart);
+router.get('/get',adminAuth,getCarts);
+
 
 module.exports = router;
