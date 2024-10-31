69b9fe8898b1af1d09fff2bae12aa60a2ba96e14
ZeroWilHero <ict21018@std.uwu.ac.lk>
2024-11-01 02:21:03 +0530

    order deletion end point has been created

--- routes/order/order.routes.js
+++ routes/order/order.routes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const createOrder = require('./controllers/createOrder');
 const getOrder = require('./controllers/getOrder').getOrder;
 const getOrders = require('./controllers/getOrder').getOrders;
+const deleteOrder = require('./controllers/deleteOrder');
 
 router.post('/add',createOrder);
 router.get('/get',getOrders);
 router.get('/get/:id',getOrder);
+router.delete('/delete/:id',deleteOrder);
 
 module.exports = router;
